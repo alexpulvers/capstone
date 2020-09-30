efaffcc3deb4807922d33d188d397e2832c7000d
apul8377 <apulvers@my.chemeketa.edu>
2020-09-29 21:06:13 -0700

    added firebase and components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Capstone Project</h1>;
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
